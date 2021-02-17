05a3c18ed63fd02b0e041f45b267c9b8d0602b41
Colin White <clwhite814@gmail.com>
2021-02-17 13:56:58 -0600

    we've connected to the URL

--- HttpClientDemo/Models/WeatherForecastModel.cs
+++ HttpClientDemo/Models/WeatherForecastModel.cs
@@ -7,29 +7,11 @@ namespace HttpClientDemo.Models
 {
     public class WeatherForecastModel
     {
-        public DayForecastModel[] consolidated_weather { get; set; }
-        public DateTime sun_rise { get; set; }
-        public DateTime sun_set { get; set; }
-        public string title { get; set; }
-        public string timezone { get; set; }
-    }
-
-    public class DayForecastModel
-    {
-        public string weather_state_name { get; set; }
-        public string weather_state_abbr { get; set; }
-        public string wind_direction_compass { get; set; }
-        public DateTime created { get; set; }
-        public string applicable_date { get; set; }
-        public float min_temp { get; set; }
-        public float max_temp { get; set; }
-        public float the_temp { get; set; }
-        public float wind_speed { get; set; }
-        public float wind_direction { get; set; }
-        public float air_pressure { get; set; }
-        public int humidity { get; set; }
-        public float visibility { get; set; }
-        public int predictability { get; set; }
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; } // sunRise
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
     }
 
 }
